13c55591bfd2e68aa05b87e69361b126ad571603
Michael Alfano <malfano3@schools.nyc.gov>
2020-09-03 19:04:31 -0400

    created and upadted PythonFoo.py

---
+++ 00-py_sandbox/PythonFoo.py
@@ -0,0 +1,31 @@
+# print statement
+print("Hello world!")
+
+# defining variables
+a = 1
+b, c = 2, 3
+
+print("a + b + c = " + str(a + b))
+
+print(str(a) + " + " + str(b) + " + " + str(c) + " = " + str(a + b + c))
+
+def sampleFunction():
+    print("Inside the function")
+    # everything indented will be in function
+# function will stop once line is no longer indented
+
+# create list
+list1 = [1, "hello", 3.14] #1-dimensional
+list2 = [["Mets", 2],["Yankees", 27]] # 2-dimensional
+
+print(list1[2])
+print(list2[1][0])
+
+user_name = input("What is your name?")
+def birthday_song(user1):
+    print("HBD2U, \n HBD2U, \n HBD dear " + str(user_name) + "!\n HBD2U")
+birthday_song(user_name)
+
+'''
+BLOCK QUOTE
+'''
